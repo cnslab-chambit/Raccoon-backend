e62169e93ddf8094204b377d647e4d3c30464fad
sung_jee <lsvk3577@naver.com>
2023-03-12 16:09:15 +0900

    [build] flutterWebAuth 로 변경

--- raccoon-api/src/main/java/kwu/raccoonapi/controller/api/user/UserSignUpController.java
+++ raccoon-api/src/main/java/kwu/raccoonapi/controller/api/user/UserSignUpController.java
@@ -17,12 +17,12 @@ import java.io.IOException;
 @Slf4j
 public class UserSignUpController {
     private final UserFacadeService userFacadeService;
-//    @GetMapping("/login/oauth2/code/kakao")
-//    public ApiResponse<?> signUpWithKakaoLocal(@RequestParam String code){
-//        return ApiResponse.ok(userFacadeService.signupWithOauth(code,VendorType.KAKAO));
-//    }
-
     @GetMapping("/login/oauth2/code/kakao")
+    public ApiResponse<?> signUpWithKakaoLocal(@RequestParam String code){
+        return ApiResponse.ok(userFacadeService.signupWithOauth(code,VendorType.KAKAO));
+    }
+
+    @GetMapping("/login/oauth2/schema/kakao")
     public void aaa(@RequestParam String code, HttpServletResponse response) throws IOException {
         response.sendRedirect("com.raccoon.sign://success?code="+code);
     }
