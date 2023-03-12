705202503c1977cf3f8d00c3a5a3616613fed7c7
seminss <2007ksm@naver.com>
2023-03-12 14:27:01 +0900

    yml 파일 ignore 됐는지 테스트

--- raccoon-api/src/main/java/kwu/raccoonapi/RaccoonApiApplication.java
+++ raccoon-api/src/main/java/kwu/raccoonapi/RaccoonApiApplication.java
@@ -10,6 +10,7 @@ public class RaccoonApiApplication {
     public static void main(String[] args) {
         System.setProperty("spring.config.name","application-api,application-domain,application-infra");
         SpringApplication.run(RaccoonApiApplication.class, args);
+
     }
 
 }
