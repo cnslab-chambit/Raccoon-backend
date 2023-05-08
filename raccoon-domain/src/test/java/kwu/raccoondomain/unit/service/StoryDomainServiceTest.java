37b34a3e9cdde27482b350a2b4c9869daca0ca59
sung_jee <lsvk3577@naver.com>
2023-05-08 18:53:46 +0900

    FileFactory+storyDomain 테스트

--- raccoon-domain/src/test/java/kwu/raccoondomain/unit/service/StoryDomainServiceTest.java
+++ raccoon-domain/src/test/java/kwu/raccoondomain/unit/service/StoryDomainServiceTest.java
@@ -1,11 +1,30 @@
 package kwu.raccoondomain.unit.service;
 
+import kwu.raccoondomain.dto.story.StoryCreateDto;
+import kwu.raccoondomain.persistence.domain.story.Story;
+import kwu.raccoondomain.persistence.domain.user.UserProfile;
+import kwu.raccoondomain.persistence.repository.story.StoryQueryRepository;
+import kwu.raccoondomain.persistence.repository.story.StoryRepository;
 import kwu.raccoondomain.service.story.StoryDomainService;
 import kwu.raccoondomain.service.story.StoryLikeDomainService;
+import kwu.raccoondomain.unit.fixture.FileFactory;
+import kwu.raccoondomain.unit.fixture.story.StoryFactory;
+import kwu.raccoondomain.unit.fixture.user.userprofile.UserProfileFactory;
+import kwu.raccooninfra.service.s3.S3Service;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 public class StoryDomainServiceTest {
@@ -13,5 +32,40 @@ public class StoryDomainServiceTest {
     @InjectMocks
     private StoryDomainService storyDomainService;
 
+    @Mock
+    private StoryRepository storyRepository;
 
+    @Mock
+    private S3Service s3Service;
+
+    @Mock
+    StoryQueryRepository storyQueryRepository;
+
+    @DisplayName("사용자는 스토리를 등록할 수 있따.")
+    @Test
+    void create_Story_LoginUser(){
+        // given
+        UserProfile userProfile = UserProfileFactory.userProfile(1L);
+        StoryCreateDto dto = StoryCreateDto.of(userProfile,"testContent", FileFactory.getTestImage1());
+
+        Story story = Story.of(userProfile,extractImageUrlFrom(dto),dto.getContents());
+
+        given(storyRepository.save(any(Story.class))).willReturn(story);
+
+        // when
+        Story ret = storyDomainService.create(dto);
+
+        // then
+        assertThat(ret).isNotNull();
+
+
+        verify(storyRepository,times(1))
+                .save(any(Story.class));
+        
+    }
+
+
+    private String extractImageUrlFrom(StoryCreateDto storyCreateDto){
+        return String.format("https://testImage./%s",storyCreateDto.getStoryImage().getName());
+    }
 }
--- raccoon-domain/src/test/java/kwu/raccoondomain/unit/service/StoryLikeDomainServiceTest.java
+++ raccoon-domain/src/test/java/kwu/raccoondomain/unit/service/StoryLikeDomainServiceTest.java
@@ -1,22 +1,32 @@
 package kwu.raccoondomain.unit.service;
 
 import kwu.raccoondomain.persistence.domain.story.Story;
+import kwu.raccoondomain.persistence.domain.story.like.Like;
 import kwu.raccoondomain.persistence.domain.user.UserProfile;
 import kwu.raccoondomain.persistence.repository.story.like.StoryLikeRepository;
 import kwu.raccoondomain.service.story.StoryLikeDomainService;
 import kwu.raccoondomain.unit.fixture.story.StoryFactory;
 import kwu.raccoondomain.unit.fixture.user.userprofile.UserProfileFactory;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 public class StoryLikeDomainServiceTest {
@@ -34,5 +44,54 @@ public class StoryLikeDomainServiceTest {
     void setUp(){
         story1 = StoryFactory.mockStory();
     }
+
+    @DisplayName("사용자의 좋아요 요청")
+    @Nested
+    public class story_like{
+        @DisplayName("좋아요 안한 상태라면 좋아요 등록")
+        @Test
+        void like(){
+            // given
+            UserProfile userProfile = UserProfileFactory.userProfile(1L);
+            Story story = StoryFactory.mockStory();
+
+            given(storyLikeRepository.storyLikeExist(userProfile,story)).willReturn(Optional.ofNullable(null));
+
+            // when
+            Boolean likeStatus = storyLikeDomainService.toggleStoryLike(userProfile,story);
+
+            // then
+            assertThat(likeStatus).isTrue();
+            assertThat(story.getLikes().getLikes().stream().filter(like -> like.getStory()==story &&
+                    like.getUserProfile()==userProfile).findAny()).isNotEmpty();
+
+            verify(storyLikeRepository,times(1))
+                    .storyLikeExist(userProfile,story);
+        }
+
+        @DisplayName("좋아요 한 상태라면 좋아요 해제")
+        @Test
+        void unlike(){
+            // given
+            UserProfile userProfile = UserProfileFactory.userProfile(1L);
+            Story story = StoryFactory.mockStory();
+
+            Like like = new Like(story,userProfile);
+
+            story.like(userProfile);
+
+            given(storyLikeRepository.storyLikeExist(userProfile,story)).willReturn(Optional.of(like));
+
+            // when
+            Boolean likeStatus = storyLikeDomainService.toggleStoryLike(userProfile,story);
+
+            // then
+            assertThat(likeStatus).isFalse();
+
+            verify(storyLikeRepository,times(1))
+                    .storyLikeExist(userProfile,story);
+        }
+    }
+
 
 }
