f8e04647730dc2cfedcf6c37c7120f74eace3b08
sung_jee <lsvk3577@naver.com>
2023-05-22 16:10:15 +0900

    [build]

--- raccoon-infra/src/main/java/kwu/raccooninfra/config/CommonConfig.java
+++ raccoon-infra/src/main/java/kwu/raccooninfra/config/CommonConfig.java
@@ -4,10 +4,10 @@ import org.axonframework.amqp.eventhandling.RoutingKeyResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
-public class CommonConfig {
-    @Bean
-    public RoutingKeyResolver routingKeyResolver(){
-        return new CustomRoutingKeyResolver();
-    }
-}
+//@Configuration
+//public class CommonConfig {
+//    @Bean
+//    public RoutingKeyResolver routingKeyResolver(){
+//        return new CustomRoutingKeyResolver();
+//    }
+//}
