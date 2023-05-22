c0e16b4856c60ce7bccfef79401c69e5d107ca97
sung_jee <lsvk3577@naver.com>
2023-05-22 15:45:05 +0900

    a

--- raccoon-chatting/src/main/java/raccoon/raccoonchatting/RaccoonChattingApplication.java
+++ raccoon-chatting/src/main/java/raccoon/raccoonchatting/RaccoonChattingApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RaccoonChattingApplication {
     public static void main(String[] args) {
+        System.setProperty("spring.config.name","application-chatting.yml");
         SpringApplication.run(RaccoonChattingApplication.class, args);
     }
 }
---
+++ raccoon-infra/src/main/java/kwu/raccooninfra/config/CommonConfig.java
@@ -0,0 +1,13 @@
+package kwu.raccooninfra.config;
+
+import org.axonframework.amqp.eventhandling.RoutingKeyResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CommonConfig {
+    @Bean
+    public RoutingKeyResolver routingKeyResolver(){
+        return new CustomRoutingKeyResolver();
+    }
+}
