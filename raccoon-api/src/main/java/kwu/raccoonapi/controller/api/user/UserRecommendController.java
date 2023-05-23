3ca8a0eba5b8131c3ba2cc44902a5c3414884cfd
sung_jee <lsvk3577@naver.com>
2023-05-23 14:25:56 +0900

    [build] 에잇

--- raccoon-api/src/main/java/kwu/raccoonapi/controller/api/user/UserRecommendController.java
+++ raccoon-api/src/main/java/kwu/raccoonapi/controller/api/user/UserRecommendController.java
@@ -1,10 +1,14 @@
 package kwu.raccoonapi.controller.api.user;
 
+import kwu.raccoonapi.dto.ApiResponse;
+import kwu.raccoonapi.dto.user.response.UserProfileDetailsResponse;
 import kwu.raccoonapi.facade.user.UserProfileFacadeService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -13,7 +17,7 @@ public class UserRecommendController {
     private final UserProfileFacadeService userProfileFacadeService;
 
     @GetMapping("/user/recommend")
-    public void getRecommendation(){
-        userProfileFacadeService.getRecommendation();
+    public ApiResponse<List<UserProfileDetailsResponse>> getRecommendation(){
+        return ApiResponse.ok(userProfileFacadeService.getRecommendation());
     }
 }
